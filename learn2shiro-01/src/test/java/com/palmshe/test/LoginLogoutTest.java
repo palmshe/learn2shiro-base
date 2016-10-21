e70dd851cf71a92367afbf058366367a83b45a3c
palmshe <palmshe@foxmailcom>
2016-10-21 12:05:19 +0800

    test default jdbc realm

--- learn2shiro-01/src/test/java/com/palmshe/test/LoginLogoutTest.java
+++ learn2shiro-01/src/test/java/com/palmshe/test/LoginLogoutTest.java
@@ -103,4 +103,33 @@ public class LoginLogoutTest {
 		//退出
 		subject.logout();
 	}
+	
+	/**
+	 * 通过配置shiro04.ini配置文件构建出shiro管理结构
+	 * Realm域使用默认的jdbcRealm，需要指定数据源
+	 * 数据库中字段根绝jdbcRealm源码构建，参考learn2shiro-01/src/test/resources/jdbcRealm.sql
+	 * 进行登录、登出测试
+	 */
+	@Test
+	public void testLoginLogout04(){
+		//通过配置文件初始化SecurityManager工厂
+		Factory<SecurityManager> factory = new IniSecurityManagerFactory("classpath:shiro04.ini");
+		//创建SecurityManager实例，并绑定到SecurityUtils工具类上，做全局使用工具
+		SecurityManager manager= factory.getInstance();
+		SecurityUtils.setSecurityManager(manager);
+		//获取Subject身份对象，该对象是源数据集合对象
+		Subject subject= SecurityUtils.getSubject();
+		//任意指定身份信息，构建出认证token
+		UsernamePasswordToken token= new UsernamePasswordToken("admin", "123456");
+		//登录，进行认证，此时通过jdbcRealm进行认证
+		try {
+			subject.login(token);
+		} catch (AuthenticationException e) {
+			e.printStackTrace();
+		}
+		//断言是否登录认证通过
+		Assert.assertEquals(true, subject.isAuthenticated());//断言失败后，后面的程序不执行
+		//退出
+		subject.logout();
+	}
 }
