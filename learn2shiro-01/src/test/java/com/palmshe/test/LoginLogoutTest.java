223140215cbb663ae56b1c75668f887255652c6b
palmshe <palmshe@foxmailcom>
2016-10-21 10:55:51 +0800

    test two customized realms

--- learn2shiro-01/src/test/java/com/palmshe/test/LoginLogoutTest.java
+++ learn2shiro-01/src/test/java/com/palmshe/test/LoginLogoutTest.java
@@ -73,4 +73,34 @@ public class LoginLogoutTest {
 		//退出
 		subject.logout();
 	}
+	
+	/**
+	 * 通过配置shiro03.ini配置文件构建出shiro管理结构
+	 * Realm域使用两个自定义palmsheRealm01，palmsheRealm02
+	 * 进行登录、登出测试
+	 */
+	@Test
+	public void testLoginLogout03(){
+		//通过配置文件初始化SecurityManager工厂
+		Factory<SecurityManager> factory = new IniSecurityManagerFactory("classpath:shiro03.ini");
+		//创建SecurityManager实例，并绑定到SecurityUtils工具类上，做全局使用工具
+		SecurityManager manager= factory.getInstance();
+		SecurityUtils.setSecurityManager(manager);
+		//获取Subject身份对象，该对象是源数据集合对象
+		Subject subject= SecurityUtils.getSubject();
+		//任意指定身份信息，构建出认证token
+		//定义多个认证策略后，默认的多个策略间的作用关系为Or关系，表示只要一个通过认证，则认证通过，否则抛出AuthenticationException
+		UsernamePasswordToken token= new UsernamePasswordToken("songxiong", "123");
+		//UsernamePasswordToken token= new UsernamePasswordToken("admin", "123456");
+		//登录，进行认证，此时通过自定义认证策略palmsheRealm01
+		try {
+			subject.login(token);
+		} catch (AuthenticationException e) {
+			e.printStackTrace();
+		}
+		//断言是否登录认证通过
+		Assert.assertEquals(true, subject.isAuthenticated());//断言失败后，后面的程序不执行
+		//退出
+		subject.logout();
+	}
 }
---
+++ learn2shiro-01/src/test/java/com/palmshe/test/realm/PalmsheRealm02.java
@@ -0,0 +1,50 @@
+package com.palmshe.test.realm;
+
+import org.apache.shiro.authc.AuthenticationException;
+import org.apache.shiro.authc.AuthenticationInfo;
+import org.apache.shiro.authc.AuthenticationToken;
+import org.apache.shiro.authc.IncorrectCredentialsException;
+import org.apache.shiro.authc.SimpleAuthenticationInfo;
+import org.apache.shiro.authc.UnknownAccountException;
+import org.apache.shiro.authc.UsernamePasswordToken;
+import org.apache.shiro.realm.Realm;
+
+/**
+ * 自定义Realm，需要实现Realm
+ * @author xiong.song
+ */
+public class PalmsheRealm02 implements Realm {
+
+	@Override
+	public AuthenticationInfo getAuthenticationInfo(AuthenticationToken token) throws AuthenticationException {
+		// 返回认证信息
+		String userName = (String) token.getPrincipal();// 获取token传入的身份，可以传入null
+		String pwd = new String((char[]) token.getCredentials());// 获取token传入的凭证，通过调试发现凭证传入后为[c类型，可以传入null
+		// 定义认证策略
+		if (!"songxiong".equals(userName)) {
+			// 抛出未知用户异常
+			throw new UnknownAccountException("用户名/密码不正确");// 防止恶意轮询用户名
+		}
+		if (!"123".equals(pwd)) {
+			// 抛出凭证不正确异常
+			throw new IncorrectCredentialsException("用户名/密码不正确");// 防止恶意轮询用户名
+		}
+		// 构造一个认证信息返回
+		return new SimpleAuthenticationInfo(userName, pwd, getName());
+	}
+
+	@Override
+	public String getName() {
+		// 返回Realm名称
+		return "PalmsheRealm01";
+	}
+
+	@Override
+	public boolean supports(AuthenticationToken token) {
+		// 定义支持token类型
+		return token instanceof UsernamePasswordToken;
+		// 如果返回false，将抛出异常UnsupportedTokenException，也属于验证异常
+		//return false;
+	}
+
+}
